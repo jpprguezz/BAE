bd1350d5e8bc09917a1d8ba3b968e1cfd12ee378
José Manuel Peña Rodríguez <145053972+jpprguezz@users.noreply.github.com>
2024-02-03 16:21:52 +0000

    Hecha parcialmente la tarea

--- Unidad-5/Tarea-7-SQLite3-(Expresiones_Regulares)/consultas-bbdd.sql
+++ Unidad-5/Tarea-7-SQLite3-(Expresiones_Regulares)/consultas-bbdd.sql
@@ -1,18 +1,104 @@
 -- Obtener todos los clientes.
+select * from Clientes;
 /**
+┌────┬─────────────────┬───────────────────────────┐
+│ id │     nombre      │           email           │
+├────┼─────────────────┼───────────────────────────┤
+│ 1  │ Juan Pérez      │ juan@example.com          │
+│ 2  │ María Gómez     │ maria@example.com         │
+│ 3  │ Carlos López    │ carlos@example.com        │
+│ 4  │ Ana Rodríguez   │ ana@example.com           │
+│ 5  │ Luisa Martínez  │ luisa@example.com         │
+│ 6  │ Pedro Sánchez   │ pedro@example.com         │
+│ 7  │ Laura García    │ laura@example.com         │
+│ 8  │ Miguel Martín   │ miguel@example.com        │
+│ 9  │ Elena González  │ elena@example.com         │
+│ 10 │ David Torres    │ david@example.com         │
+│ 11 │ Sofía Ruiz      │ sofia@example.com         │
+│ 12 │ Javier López    │ javier@example.com        │
+│ 13 │ Carmen Vargas   │ carmen@example.com        │
+│ 14 │ Daniel Muñoz    │ daniel@example.com        │
+│ 15 │ Isabel Serrano  │ isabel@example.com        │
+│ 16 │ Alejandro Muñoz │ alejandro@example.com     │
+│ 17 │ Raquel Herrera  │ raquel@example.com        │
+│ 18 │ Francisco Mora  │ francisco@example.com     │
+│ 19 │ Marina Díaz     │ marina@example.com        │
+│ 20 │ Antonio Jiménez │ antonio@example.com       │
+│ 21 │ Beatriz Romero  │ beatriz@example.com       │
+│ 22 │ Carlos Gómez    │ carlos.gomez@example.com  │
+│ 23 │ Clara Sánchez   │ clara.sanchez@example.com │
+│ 24 │ Andrés Martínez │ andres@example.com        │
+│ 25 │ Lucía Díaz      │ lucia@example.com         │
+│ 26 │ Mario Serrano   │ mario@example.com         │
+│ 27 │ Eva Torres      │ eva.torres@example.com    │
+│ 28 │ Roberto Ruiz    │ roberto@example.com       │
+│ 29 │ Celia García    │ celia@example.com         │
+└────┴─────────────────┴───────────────────────────┘
 **/
 -- Obtener la cantidad total de productos en todos los pedidos
+select sum(cantidad) from Pedidos;
 /**
+┌───────────────┐
+│ sum(cantidad) │
+├───────────────┤
+│ 54            │
+└───────────────┘
 **/
 -- Obtener el precio promedio de los productos:
+select AVG(precio) from Productos;
 /**
+┌──────────────────┐
+│   AVG(precio)    │
+├──────────────────┤
+│ 188.294285714286 │
+└──────────────────┘
 **/
 -- Obtener los clientes que tienen un email válido (contiene '@'):
+select nombre, email from Clientes where email regexp '@';
 /**
+┌─────────────────┬───────────────────────────┐
+│     nombre      │           email           │
+├─────────────────┼───────────────────────────┤
+│ Juan Pérez      │ juan@example.com          │
+│ María Gómez     │ maria@example.com         │
+│ Carlos López    │ carlos@example.com        │
+│ Ana Rodríguez   │ ana@example.com           │
+│ Luisa Martínez  │ luisa@example.com         │
+│ Pedro Sánchez   │ pedro@example.com         │
+│ Laura García    │ laura@example.com         │
+│ Miguel Martín   │ miguel@example.com        │
+│ Elena González  │ elena@example.com         │
+│ David Torres    │ david@example.com         │
+│ Sofía Ruiz      │ sofia@example.com         │
+│ Javier López    │ javier@example.com        │
+│ Carmen Vargas   │ carmen@example.com        │
+│ Daniel Muñoz    │ daniel@example.com        │
+│ Isabel Serrano  │ isabel@example.com        │
+│ Alejandro Muñoz │ alejandro@example.com     │
+│ Raquel Herrera  │ raquel@example.com        │
+│ Francisco Mora  │ francisco@example.com     │
+│ Marina Díaz     │ marina@example.com        │
+│ Antonio Jiménez │ antonio@example.com       │
+│ Beatriz Romero  │ beatriz@example.com       │
+│ Carlos Gómez    │ carlos.gomez@example.com  │
+│ Clara Sánchez   │ clara.sanchez@example.com │
+│ Andrés Martínez │ andres@example.com        │
+│ Lucía Díaz      │ lucia@example.com         │
+│ Mario Serrano   │ mario@example.com         │
+│ Eva Torres      │ eva.torres@example.com    │
+│ Roberto Ruiz    │ roberto@example.com       │
+│ Celia García    │ celia@example.com         │
+└─────────────────┴───────────────────────────┘
 **/
 
 -- Obtener el producto más caro.
+select nombre, max(precio) from Productos;
 /**
+┌────────┬─────────────┐
+│ nombre │ max(precio) │
+├────────┼─────────────┤
+│ Laptop │ 1200.0      │
+└────────┴─────────────┘
 **/
 
 -- Obtener los pedidos realizados en febrero de 2024.
