fd3c531bdff20a4e4bda0fb8eb4afcf9abdb71ea
José Manuel Peña Rodríguez <145053972+jpprguezz@users.noreply.github.com>
2024-02-18 08:31:08 +0000

    Arreglado espaciado

--- Unidad-5/Tarea-11-SQLite3-Repaso-1/consultas-bbdd.sql
+++ Unidad-5/Tarea-11-SQLite3-Repaso-1/consultas-bbdd.sql
@@ -1,88 +1,89 @@
 ** Consultas SQL sobre una tabla
-    -- Devuelve un listado con el código de oficina y la ciudad donde hay oficinas.
-    /**
-    **/
-    -- Devuelve un listado con la ciudad y el teléfono de las oficinas de España.
-    /**
-    **/
-    -- Devuelve un listado con el nombre, apellidos y email de los empleados cuyo jefe tiene un código de jefe igual a 7.
-    /**
-    **/
-    -- Devuelve el nombre del puesto, nombre, apellidos y email del jefe de la empresa.
-    /**
-    **/
-    -- Devuelve un listado con el nombre, apellidos y puesto de aquellos empleados que no sean representantes de ventas.
-    /**
-    **/
-    -- Devuelve un listado con el nombre de los todos los clientes españoles.
-    /**
-    **/
-    -- Devuelve un listado con los distintos estados por los que puede pasar un pedido.
-    /**
-    **/
-    -- Devuelve un listado con el código de cliente de aquellos clientes que realizaron algún pago en 2008. Tenga en cuenta que deberá eliminar aquellos códigos de cliente que aparezcan repetidos.
-    /**
-    **/
-    -- Devuelve un listado con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos que no han sido entregados a tiempo.
-    /**
-    **/
-    -- Devuelve un listado con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos cuya fecha de entrega ha sido al menos dos días antes de la fecha esperada.
-    /**
-    **/
-    -- Devuelve un listado de todos los pedidos que fueron rechazados en 2009.
-    /**
-    **/
-    -- Devuelve un listado de todos los pedidos que han sido entregados en el mes de enero de cualquier año.
-    /**
-    **/
-    -- Devuelve un listado con todos los pagos que se realizaron en el año 2008 mediante Paypal. Ordene el resultado de mayor a menor.
-    /**
-    **/
-    -- Devuelve un listado con todas las formas de pago que aparecen en la tabla pago. Tenga en cuenta que no deben aparecer formas de pago repetidas.
-    /**
-    **/
-    -- Devuelve un listado con todos los productos que pertenecen a la gama Ornamentales y que tienen más de 100 unidades en stock. El listado deberá estar ordenado por su precio de venta, mostrando en primer lugar los de mayor precio.
-    /**
-    **/
-    -- Devuelve un listado con todos los clientes que sean de la ciudad de Madrid y cuyo representante de ventas tenga el código de empleado 11 o 30.
-    /**
-    **/
+-- Devuelve un listado con el código de oficina y la ciudad donde hay oficinas.
+/**
+
+**/
+-- Devuelve un listado con la ciudad y el teléfono de las oficinas de España.
+/**
+**/
+-- Devuelve un listado con el nombre, apellidos y email de los empleados cuyo jefe tiene un código de jefe igual a 7.
+/**
+**/
+-- Devuelve el nombre del puesto, nombre, apellidos y email del jefe de la empresa.
+/**
+**/
+-- Devuelve un listado con el nombre, apellidos y puesto de aquellos empleados que no sean representantes de ventas.
+/**
+**/
+-- Devuelve un listado con el nombre de los todos los clientes españoles.
+/**
+**/
+-- Devuelve un listado con los distintos estados por los que puede pasar un pedido.
+/**
+**/
+-- Devuelve un listado con el código de cliente de aquellos clientes que realizaron algún pago en 2008. Tenga en cuenta que deberá eliminar aquellos códigos de cliente que aparezcan repetidos.
+/**
+**/
+-- Devuelve un listado con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos que no han sido entregados a tiempo.
+/**
+**/
+-- Devuelve un listado con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos cuya fecha de entrega ha sido al menos dos días antes de la fecha esperada.
+/**
+**/
+-- Devuelve un listado de todos los pedidos que fueron rechazados en 2009.
+/**
+**/
+-- Devuelve un listado de todos los pedidos que han sido entregados en el mes de enero de cualquier año.
+/**
+**/
+-- Devuelve un listado con todos los pagos que se realizaron en el año 2008 mediante Paypal. Ordene el resultado de mayor a menor.
+/**
+**/
+-- Devuelve un listado con todas las formas de pago que aparecen en la tabla pago. Tenga en cuenta que no deben aparecer formas de pago repetidas.
+/**
+**/
+-- Devuelve un listado con todos los productos que pertenecen a la gama Ornamentales y que tienen más de 100 unidades en stock. El listado deberá estar ordenado por su precio de venta, mostrando en primer lugar los de mayor precio.
+/**
+**/
+-- Devuelve un listado con todos los clientes que sean de la ciudad de Madrid y cuyo representante de ventas tenga el código de empleado 11 o 30.
+/**
+**/
 
 
 ** Consultas multitabla (Where para unir tablas)
-    --Obtén un listado con el nombre de cada cliente y el nombre y apellido de su representante de ventas.
-    /**
-    **/
-    --Muestra el nombre de los clientes que hayan realizado pagos junto con el nombre de sus representantes de ventas.
-    /**
-    **/
-    --Muestra el nombre de los clientes que no hayan realizado pagos junto con el nombre de sus representantes de ventas.
-    /**
-    **/
-    --Devuelve el nombre de los clientes que han hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
-    /**
-    **/
-    --Devuelve el nombre de los clientes que no hayan hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
-    /**
-    **/
-    --Lista la dirección de las oficinas que tengan clientes en Fuenlabrada.
-    /**
-    **/
-    --Devuelve el nombre de los clientes y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
-    /**
-    **/
-    --Devuelve un listado con el nombre de los empleados junto con el nombre de sus jefes.
-    /**
-    **/
-    --Devuelve un listado que muestre el nombre de cada empleados, el nombre de su jefe y el nombre del jefe de sus jefe.
-    /**
-    **/
-    --Devuelve el nombre de los clientes a los que no se les ha entregado a tiempo un pedido.
-    /**
-    **/
-    --Devuelve un listado de las diferentes gamas de producto que ha comprado cada cliente.
-    /**
-    **/
+ --Obtén un listado con el nombre de cada cliente y el nombre y apellido de su representante de ventas.
+ /**
+ **/
+ --Muestra el nombre de los clientes que hayan realizado pagos junto con el nombre de sus representantes de ventas.
+ /**
+ **/
+ --Muestra el nombre de los clientes que no hayan realizado pagos junto con el nombre de sus representantes de ventas.
+ /**
+ **/
+ --Devuelve el nombre de los clientes que han hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+ /**
+ **/
+ --Devuelve el nombre de los clientes que no hayan hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+ /**
+ **/
+ --Lista la dirección de las oficinas que tengan clientes en Fuenlabrada.
+ /**
+ **/
+ --Devuelve el nombre de los clientes y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+ /**
+ **/
+ --Devuelve un listado con el nombre de los empleados junto con el nombre de sus jefes.
+ /**
+ **/
+ --Devuelve un listado que muestre el nombre de cada empleados, el nombre de su jefe y el nombre del jefe de sus jefe.
+ /**
+ **/
+ --Devuelve el nombre de los clientes a los que no se les ha entregado a tiempo un pedido.
+ /**
+ **/
+ --Devuelve un listado de las diferentes gamas de producto que ha comprado cada cliente.
+ /**
+ **/
 
 
 ** Consultas multitabla (inner join)
