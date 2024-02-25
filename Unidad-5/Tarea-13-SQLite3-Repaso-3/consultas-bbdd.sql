b00a22da3199e2b2a9a822eb04faacd7dc44856e
José Manuel Peña Rodríguez <145053972+jpprguezz@users.noreply.github.com>
2024-02-25 10:41:45 +0000

    hecha parcialmente la tarea

--- Unidad-5/Tarea-13-SQLite3-Repaso-3/consultas-bbdd.sql
+++ Unidad-5/Tarea-13-SQLite3-Repaso-3/consultas-bbdd.sql
@@ -1,17 +1,89 @@
 ** Consultas de una única tabla
 
 -- Devuelve un listado con el primer apellido, segundo apellido y el nombre de todos los alumnos. El listado deberá estar ordenado alfabéticamente de menor a mayor por el primer apellido, segundo apellido y nombre.
--- Averigua el nombre y los dos apellidos de los alumnos que no han dado de alta su número de teléfono en la base de datos.
--- Devuelve el listado de los alumnos que nacieron en 1999.
--- Devuelve el listado de profesores que no han dado de alta su número de teléfono en la base de datos y además su nif termina en K.
--- Devuelve el listado de las asignaturas que se imparten en el primer cuatrimestre, en el tercer curso del grado que tiene el identificador 7.
+select apellido1, apellido2, nombre from persona order by apellido1, apellido2, nombre desc;
 
+┌────────────┬────────────┬───────────┐
+│ apellido1  │ apellido2  │  nombre   │
+├────────────┼────────────┼───────────┤
+│ Domínguez  │ Guerrero   │ Antonio   │
+│ Fahey      │ Considine  │ Antonio   │
+│ Gea        │ Ruiz       │ Sonia     │
+│ Gutiérrez  │ López      │ Juan      │
+│ Hamill     │ Kozey      │ Manolo    │
+│ Heller     │ Pagac      │ Pedro     │
+│ Herman     │ Pacocha    │ Daniel    │
+│ Hernández  │ Martínez   │ Irene     │
+│ Herzog     │ Tremblay   │ Ramón     │
+│ Kohler     │ Schoen     │ Alejandro │
+│ Koss       │ Bayer      │ José      │
+│ Lakin      │ Yundt      │ Inma      │
+│ Lemke      │ Rutherford │ Cristina  │
+│ Monahan    │ Murray     │ Micaela   │
+│ Ramirez    │ Gea        │ Zoe       │
+│ Ruecker    │ Upton      │ Guillermo │
+│ Saez       │ Vega       │ Juan      │
+│ Schmidt    │ Fisher     │ David     │
+│ Schowalter │ Muller     │ Francesca │
+│ Spencer    │ Lakin      │ Esther    │
+│ Stiedemann │ Morissette │ Alfredo   │
+│ Streich    │ Hirthe     │ Carmen    │
+│ Strosin    │ Turcotte   │ Ismael    │
+│ Sánchez    │ Pérez      │ Salvador  │
+└────────────┴────────────┴───────────┘
+-- Averigua el nombre y los dos apellidos de los alumnos que no han dado de alta su número de teléfono en la base de datos.
+select nombre, apellido1, apellido2, telefono from persona where telefono is null;
+
+┌───────────┬────────────┬───────────┬──────────┐
+│  nombre   │ apellido1  │ apellido2 │ telefono │
+├───────────┼────────────┼───────────┼──────────┤
+│ Pedro     │ Heller     │ Pagac     │          │
+│ Ismael    │ Strosin    │ Turcotte  │          │
+│ Esther    │ Spencer    │ Lakin     │          │
+│ Carmen    │ Streich    │ Hirthe    │          │
+│ Antonio   │ Fahey      │ Considine │          │
+│ Guillermo │ Ruecker    │ Upton     │          │
+│ Francesca │ Schowalter │ Muller    │          │
+└───────────┴────────────┴───────────┴──────────┘
+-- Devuelve el listado de los alumnos que nacieron en 1999.
+select * from persona where fecha_nacimiento regexp 1999;
+
+┌────┬───────────┬─────────┬───────────┬───────────┬─────────┬─────────────────┬───────────┬──────────────────┬──────┬────────┐
+│ id │    nif    │ nombre  │ apellido1 │ apellido2 │ ciudad  │    direccion    │ telefono  │ fecha_nacimiento │ sexo │  tipo  │
+├────┼───────────┼─────────┼───────────┼───────────┼─────────┼─────────────────┼───────────┼──────────────────┼──────┼────────┤
+│ 7  │ 97258166K │ Ismael  │ Strosin   │ Turcotte  │ Almería │ C/ Neptuno      │           │ 1999/05/24       │ H    │ alumno │
+│ 22 │ 41491230N │ Antonio │ Domínguez │ Guerrero  │ Almería │ C/ Cabo de Gata │ 626652498 │ 1999/02/11       │ H    │ alumno │
+└────┴───────────┴─────────┴───────────┴───────────┴─────────┴─────────────────┴───────────┴──────────────────┴──────┴────────┘
+-- Devuelve el listado de profesores que no han dado de alta su número de teléfono en la base de datos y además su nif termina en K.
+select * from persona where telefono is null and nif regexp 'K$';
+
+┌────┬───────────┬───────────┬───────────┬───────────┬─────────┬───────────────────────────┬──────────┬──────────────────┬──────┬──────────┐
+│ id │    nif    │  nombre   │ apellido1 │ apellido2 │ ciudad  │         direccion         │ telefono │ fecha_nacimiento │ sexo │   tipo   │
+├────┼───────────┼───────────┼───────────┼───────────┼─────────┼───────────────────────────┼──────────┼──────────────────┼──────┼──────────┤
+│ 7  │ 97258166K │ Ismael    │ Strosin   │ Turcotte  │ Almería │ C/ Neptuno                │          │ 1999/05/24       │ H    │ alumno   │
+│ 16 │ 10485008K │ Antonio   │ Fahey     │ Considine │ Almería │ C/ Sierra de los Filabres │          │ 1982/03/18       │ H    │ profesor │
+│ 17 │ 85869555K │ Guillermo │ Ruecker   │ Upton     │ Almería │ C/ Sierra de Gádor        │          │ 1973/05/05       │ H    │ profesor │
+└────┴───────────┴───────────┴───────────┴───────────┴─────────┴───────────────────────────┴──────────┴──────────────────┴──────┴──────────┘
+-- Devuelve el listado de las asignaturas que se imparten en el primer cuatrimestre, en el tercer curso del grado que tiene el identificador 7.
+select * from asignatura where cuatrimestre regexp 1 and curso regexp 3 and id_grado regexp 7;
+
+┌────┬───────────────────────────────────────────┬──────────┬─────────────┬───────┬──────────────┬─────────────┬──────────┐
+│ id │                  nombre                   │ creditos │    tipo     │ curso │ cuatrimestre │ id_profesor │ id_grado │
+├────┼───────────────────────────────────────────┼──────────┼─────────────┼───────┼──────────────┼─────────────┼──────────┤
+│ 72 │ Bases moleculares del desarrollo vegetal  │ 4.5      │ obligatoria │ 3     │ 1            │             │ 7        │
+│ 73 │ Fisiología animal                         │ 4.5      │ obligatoria │ 3     │ 1            │             │ 7        │
+│ 74 │ Metabolismo y biosíntesis de biomoléculas │ 6.0      │ obligatoria │ 3     │ 1            │             │ 7        │
+│ 75 │ Operaciones de separación                 │ 6.0      │ obligatoria │ 3     │ 1            │             │ 7        │
+│ 76 │ Patología molecular de plantas            │ 4.5      │ obligatoria │ 3     │ 1            │             │ 7        │
+│ 77 │ Técnicas instrumentales básicas           │ 4.5      │ obligatoria │ 3     │ 1            │             │ 7        │
+└────┴───────────────────────────────────────────┴──────────┴─────────────┴───────┴──────────────┴─────────────┴──────────┘
 
 
 ** Consultas multitabla (Join)
 
 -- Devuelve un listado con los datos de todas las alumnas que se han matriculado alguna vez en el Grado en Ingeniería Informática (Plan 2015).
 -- Devuelve un listado con todas las asignaturas ofertadas en el Grado en Ingeniería Informática (Plan 2015).
+
 -- Devuelve un listado de los profesores junto con el nombre del departamento al que están vinculados. El listado debe devolver cuatro columnas, primer apellido, segundo apellido, nombre y nombre del departamento. El resultado estará ordenado alfabéticamente de menor a mayor por los apellidos y el nombre.
 -- Devuelve un listado con el nombre de las asignaturas, año de inicio y año de fin del curso escolar del alumno con nif 26902806M.
 -- Devuelve un listado con el nombre de todos los departamentos que tienen profesores que imparten alguna asignatura en el Grado en Ingeniería Informática (Plan 2015).
@@ -31,8 +103,8 @@
 
 
 ** Consultas resúmen (Funciones)
-Devuelve el número total de alumnas que hay.
 
+-- Devuelve el número total de alumnas que hay.
 -- Calcula cuántos alumnos nacieron en 1999.
 -- Calcula cuántos profesores hay en cada departamento. El resultado sólo debe mostrar dos columnas, una con el nombre del departamento y otra con el número de profesores que hay en ese departamento. El resultado sólo debe incluir los departamentos que tienen profesores asociados y deberá estar ordenado de mayor a menor por el número de profesores.
 -- Devuelve un listado con todos los departamentos y el número de profesores que hay en cada uno de ellos. Tenga en cuenta que pueden existir departamentos que no tienen profesores asociados. Estos departamentos también tienen que aparecer en el listado.
@@ -41,3 +113,15 @@ Devuelve el número total de alumnas que hay.
 -- Devuelve un listado que muestre el nombre de los grados y la suma del número total de créditos que hay para cada tipo de asignatura. El resultado debe tener tres columnas: nombre del grado, tipo de asignatura y la suma de los créditos de todas las asignaturas que hay de ese tipo. Ordene el resultado de mayor a menor por el número total de crédidos.
 -- Devuelve un listado que muestre cuántos alumnos se han matriculado de alguna asignatura en cada uno de los cursos escolares. El resultado deberá mostrar dos columnas, una columna con el año de inicio del curso escolar y otra con el número de alumnos matriculados.
 -- Devuelve un listado con el número de asignaturas que imparte cada profesor. El listado debe tener en cuenta aquellos profesores que no imparten ninguna asignatura. El resultado mostrará cinco columnas: id, nombre, primer apellido, segundo apellido y número de asignaturas. El resultado estará ordenado de mayor a menor por el número de asignaturas.
+
+
+
+** Subconsultas
+
+-- Devuelve todos los datos del alumno más joven.
+-- Devuelve un listado con los profesores que no están asociados a un departamento.
+-- Devuelve un listado con los departamentos que no tienen profesores asociados.
+-- Devuelve un listado con los profesores que tienen un departamento asociado y que no imparten ninguna asignatura.
+-- Devuelve un listado con las asignaturas que no tienen un profesor asignado.
+-- Devuelve un listado con todos los departamentos que no han impartido asignaturas en ningún curso escolar.
+
