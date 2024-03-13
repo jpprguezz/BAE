b027d5b1e8a8844d3a60459290a919d386b82e38
José Manuel Peña Rodríguez <145053972+jpprguezz@users.noreply.github.com>
2024-03-13 19:50:24 +0000

    Terminada tarea

--- Unidad-6/MySQL/Tarea-2-Consultas-Simples-y-Joins/consultas.sql
+++ Unidad-6/MySQL/Tarea-2-Consultas-Simples-y-Joins/consultas.sql
@@ -443,10 +443,10 @@ INSERT INTO ordenes (id_cliente, id_producto, cantidad) VALUES
         |       20 |         20 |          20 |        2 | Cliente 20 | Producto 20 |
         +----------+------------+-------------+----------+------------+-------------+
         -- Obtener el nombre de los clientes junto con el número total de órdenes que han realizado
-        select cl.nombre, count(o.id_orden) from clientes as cl join ordenes as o on o.id_cliente=cl.id_cliente group by cl.id_cliente;
+        select cl.nombre, count(o.id_orden) as total_ordenes from clientes as cl join ordenes as o on o.id_cliente=cl.id_cliente group by cl.id_cliente;
 
         +------------+-------------------+
-        | nombre     | count(o.id_orden) |
+        | nombre     | total_ordenes     |
         +------------+-------------------+
         | Cliente 1  |                 1 |
         | Cliente 2  |                 1 |
@@ -524,6 +524,114 @@ INSERT INTO ordenes (id_cliente, id_producto, cantidad) VALUES
         |       20 |         20 |          20 |        2 |          20 | Producto 20 |  200.5 |         20 | Cliente 20 | Dirección 20 |
         +----------+------------+-------------+----------+-------------+-------------+--------+------------+------------+--------------+
         -- Obtener el nombre de los productos junto con el nombre de los clientes que han realizado órdenes de esos productos, incluyendo los productos que no han sido ordenados
+        select pr.nombre, cl.nombre from clientes as cl join ordenes as o on cl.id_cliente=o.id_producto join productos as pr on pr.id_producto=o.id_producto;
+
+        +-------------+------------+
+        | nombre      | nombre     |
+        +-------------+------------+
+        | Producto 1  | Cliente 1  |
+        | Producto 2  | Cliente 2  |
+        | Producto 3  | Cliente 3  |
+        | Producto 4  | Cliente 4  |
+        | Producto 5  | Cliente 5  |
+        | Producto 6  | Cliente 6  |
+        | Producto 7  | Cliente 7  |
+        | Producto 8  | Cliente 8  |
+        | Producto 9  | Cliente 9  |
+        | Producto 10 | Cliente 10 |
+        | Producto 11 | Cliente 11 |
+        | Producto 12 | Cliente 12 |
+        | Producto 13 | Cliente 13 |
+        | Producto 14 | Cliente 14 |
+        | Producto 15 | Cliente 15 |
+        | Producto 16 | Cliente 16 |
+        | Producto 17 | Cliente 17 |
+        | Producto 18 | Cliente 18 |
+        | Producto 19 | Cliente 19 |
+        | Producto 20 | Cliente 20 |
+        +-------------+------------+
+
         -- Mostrar todas las órdenes junto con el nombre del cliente y el nombre del producto, incluyendo las órdenes sin productos
+        select o.*, cl.nombre, pr.nombre from ordenes as o left join clientes as cl on cl.id_cliente=o.id_cliente join productos as pr on pr.id_producto=o.id_producto;
+
+        +----------+------------+-------------+----------+------------+-------------+
+        | id_orden | id_cliente | id_producto | cantidad | nombre     | nombre      |
+        +----------+------------+-------------+----------+------------+-------------+
+        |        1 |          1 |           1 |        2 | Cliente 1  | Producto 1  |
+        |        2 |          2 |           2 |        1 | Cliente 2  | Producto 2  |
+        |        3 |          3 |           3 |        3 | Cliente 3  | Producto 3  |
+        |        4 |          4 |           4 |        2 | Cliente 4  | Producto 4  |
+        |        5 |          5 |           5 |        1 | Cliente 5  | Producto 5  |
+        |        6 |          6 |           6 |        2 | Cliente 6  | Producto 6  |
+        |        7 |          7 |           7 |        3 | Cliente 7  | Producto 7  |
+        |        8 |          8 |           8 |        2 | Cliente 8  | Producto 8  |
+        |        9 |          9 |           9 |        1 | Cliente 9  | Producto 9  |
+        |       10 |         10 |          10 |        2 | Cliente 10 | Producto 10 |
+        |       11 |         11 |          11 |        3 | Cliente 11 | Producto 11 |
+        |       12 |         12 |          12 |        2 | Cliente 12 | Producto 12 |
+        |       13 |         13 |          13 |        1 | Cliente 13 | Producto 13 |
+        |       14 |         14 |          14 |        2 | Cliente 14 | Producto 14 |
+        |       15 |         15 |          15 |        3 | Cliente 15 | Producto 15 |
+        |       16 |         16 |          16 |        2 | Cliente 16 | Producto 16 |
+        |       17 |         17 |          17 |        1 | Cliente 17 | Producto 17 |
+        |       18 |         18 |          18 |        2 | Cliente 18 | Producto 18 |
+        |       20 |         20 |          20 |        2 | Cliente 20 | Producto 20 |
+        |       19 |         19 |          19 |        3 | Cliente 19 | Producto 19 |
+        +----------+------------+-------------+----------+------------+-------------+
+
         -- Obtener el nombre de los clientes junto con el número total de órdenes que han realizado, incluyendo los clientes que no han realizado órdenes.
+        select cl.nombre, count(o.id_orden) as total_ordenes from clientes as cl left join ordenes as o on o.id_cliente=cl.id_cliente join productos as pr on pr.id_producto=o.id_producto group by cl.id_cliente;
+
+        +------------+---------------+
+        | nombre     | total_ordenes |
+        +------------+---------------+
+        | Cliente 1  |             1 |
+        | Cliente 2  |             1 |
+        | Cliente 3  |             1 |
+        | Cliente 4  |             1 |
+        | Cliente 5  |             1 |
+        | Cliente 6  |             1 |
+        | Cliente 7  |             1 |
+        | Cliente 8  |             1 |
+        | Cliente 9  |             1 |
+        | Cliente 10 |             1 |
+        | Cliente 11 |             1 |
+        | Cliente 12 |             1 |
+        | Cliente 13 |             1 |
+        | Cliente 14 |             1 |
+        | Cliente 15 |             1 |
+        | Cliente 16 |             1 |
+        | Cliente 17 |             1 |
+        | Cliente 18 |             1 |
+        | Cliente 19 |             1 |
+        | Cliente 20 |             1 |
+        +------------+---------------+
+
         -- Mostrar todas las órdenes con sus clientes y productos, incluyendo las órdenes y productos que no tienen información.
+        select o.*, cl.*, pr.* from ordenes as o left join productos as pr on pr.id_producto=o.id_producto join clientes as cl on cl.id_cliente=o.id_cliente;
+
+        +----------+------------+-------------+----------+------------+------------+---------------+-------------+-------------+--------+
+        | id_orden | id_cliente | id_producto | cantidad | id_cliente | nombre     | direccion     | id_producto | nombre      | precio |
+        +----------+------------+-------------+----------+------------+------------+---------------+-------------+-------------+--------+
+        |        1 |          1 |           1 |        2 |          1 | Cliente 1  | Dirección 1   |           1 | Producto 1  |  10.99 |
+        |        2 |          2 |           2 |        1 |          2 | Cliente 2  | Dirección 2   |           2 | Producto 2  |   20.5 |
+        |        3 |          3 |           3 |        3 |          3 | Cliente 3  | Dirección 3   |           3 | Producto 3  |  30.99 |
+        |        4 |          4 |           4 |        2 |          4 | Cliente 4  | Dirección 4   |           4 | Producto 4  |   40.5 |
+        |        5 |          5 |           5 |        1 |          5 | Cliente 5  | Dirección 5   |           5 | Producto 5  |  50.99 |
+        |        6 |          6 |           6 |        2 |          6 | Cliente 6  | Dirección 6   |           6 | Producto 6  |   60.5 |
+        |        7 |          7 |           7 |        3 |          7 | Cliente 7  | Dirección 7   |           7 | Producto 7  |  70.99 |
+        |        8 |          8 |           8 |        2 |          8 | Cliente 8  | Dirección 8   |           8 | Producto 8  |   80.5 |
+        |        9 |          9 |           9 |        1 |          9 | Cliente 9  | Dirección 9   |           9 | Producto 9  |  90.99 |
+        |       10 |         10 |          10 |        2 |         10 | Cliente 10 | Dirección 10  |          10 | Producto 10 |  100.5 |
+        |       11 |         11 |          11 |        3 |         11 | Cliente 11 | Dirección 11  |          11 | Producto 11 | 110.99 |
+        |       12 |         12 |          12 |        2 |         12 | Cliente 12 | Dirección 12  |          12 | Producto 12 |  120.5 |
+        |       13 |         13 |          13 |        1 |         13 | Cliente 13 | Dirección 13  |          13 | Producto 13 | 130.99 |
+        |       14 |         14 |          14 |        2 |         14 | Cliente 14 | Dirección 14  |          14 | Producto 14 |  140.5 |
+        |       15 |         15 |          15 |        3 |         15 | Cliente 15 | Dirección 15  |          15 | Producto 15 | 150.99 |
+        |       16 |         16 |          16 |        2 |         16 | Cliente 16 | Dirección 16  |          16 | Producto 16 |  160.5 |
+        |       17 |         17 |          17 |        1 |         17 | Cliente 17 | Dirección 17  |          17 | Producto 17 | 170.99 |
+        |       18 |         18 |          18 |        2 |         18 | Cliente 18 | Dirección 18  |          18 | Producto 18 |  180.5 |
+        |       19 |         19 |          19 |        3 |         19 | Cliente 19 | Dirección 19  |          19 | Producto 19 | 190.99 |
+        |       20 |         20 |          20 |        2 |         20 | Cliente 20 | Dirección 20  |          20 | Producto 20 |  200.5 |
+        +----------+------------+-------------+----------+------------+------------+---------------+-------------+-------------+--------+
+
